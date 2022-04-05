e787a86432a204da71b9556aef6c77dda6cf626f
Batistajunior <86987034+Batistajunior@users.noreply.github.com>
2022-04-05 19:20:31 -0300

    Alteração e Criação da classe pessoa

---
+++ RestApiProject/src/RestApiProject/Pessoa.java
@@ -0,0 +1,16 @@
+package RestApiProject;
+
+public class Pessoa {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	private Sobrenome
+}
